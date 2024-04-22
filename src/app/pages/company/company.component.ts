7b9895a19f8b997eda766adfa1fe551244294534
VitSmol <smolitskiy@mail.ru>
2024-04-22 06:07:31 +0300

    upd

--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -1,6 +1,6 @@
 import { Component, HostListener, OnInit, VERSION, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ImgDialogComponent } from 'src/app/shared/img-dialog/img-dialog.component';
+// import { ImgDialogComponent } from 'src/app/shared/img-dialog/img-dialog.component';
 import { BeforeSlideDetail } from 'lightgallery/lg-events';
 import { LightgalleryModule } from 'lightgallery/angular';
 import lgZoom from 'lightgallery/plugins/zoom';
@@ -65,14 +65,14 @@ constructor(
   public dialog: MatDialog
 ){}
 
-showSwipe(img: {pathMini: string, pathFull: string}) {
-  const dialog = this.dialog.open(ImgDialogComponent, {
-    data: [img, this.imgArray],
-    width: `60%`,
-    height: `auto`
-  })
+// showSwipe(img: {pathMini: string, pathFull: string}) {
+//   const dialog = this.dialog.open(ImgDialogComponent, {
+//     data: [img, this.imgArray],
+//     width: `60%`,
+//     height: `auto`
+//   })
 
-}
+// }
 
 settings = {
   width: '300px',
--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ImgDialogComponent } from 'src/app/shared/img-dialog/img-dialog.component';
 
 @Component({
   selector: 'app-contacts',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './contacts.component.sass'
 })
 export class ContactsComponent {
+  public src: string = 'assets/img/photo/'
 
+  constructor(
+    private dialog: MatDialog
+  ){}
+  showDialog(e: any, src: string) {
+    e.preventDefault();
+    const dialog = this.dialog.open(ImgDialogComponent, {
+      data: this.src + src,
+      width: '400px',
+      // height: '300px'
+    })
+  }
+  toApplication(e: any) {
+    e.preventDefault();
+  }
 }
