9ef4507d165482ad7de6a2171f228bc84a28bdc0
VitSmol <smolitskiy@mail.ru>
2024-08-03 13:39:07 +0300

    раздел новости, адаптив

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { news } from './news'
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrl: './news.component.sass'
 })
-export class NewsComponent {
+export class NewsComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    // this.showSlides(this.slideIndex)
+    // throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+  public news = news.reverse();
+  public slideIndex = 1;
 
+  plusSlides(n: number, img: string): void {
+    // this.showSlides(this.slideIndex = n)
+
+  }
+
+  // showSlides(n: number): void {
+//     let slides = document.getElementsByClassName('mySlides');
+//     console.log(slides);
+//     if (n > slides.length) {
+//       this.slideIndex = 1;
+//     }
+//     if (n < 1) {
+//       this.slideIndex = slides.length
+//     }
+//     for (let i = 0; i < slides.length; i++) {
+//       (slides[i] as HTMLElement).style.display = 'none';
+//     }
+//     (slides[this.slideIndex-1] as HTMLElement).style.display = 'block';
+
+  // }
 }
